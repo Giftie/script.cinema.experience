6b6e8739821e7962a85d1988fc60a472157cef11
Giftie <giftie61@hotmail.com>
2013-09-27 00:18:44 -0600

    Add error testing for path names

--- resources/lib/folder.py
+++ resources/lib/folder.py
@@ -1,18 +1,27 @@
 # -*- coding: utf-8 -*-
 
-import sys, os, re
+import sys, os, re, traceback
 import xbmc, xbmcvfs
 import utils
 
 def absolute_listdir( path, media_type = "files", recursive = False, contains = "" ):
     absolute_files = []
     absolute_folders = []
+    path = utils.smart_unicode( path )
     folders, files = xbmcvfs.listdir( path )
     for f in files:
         f = utils.smart_unicode( f )
         if media_type == "files":
             if not contains or ( contains and ( contains in f ) ):
-                absolute_files.append( os.path.join( path, f ) )
+                try:
+                    absolute_files.append( os.path.join( path, f ) )
+                except UnicodeError:
+                    utils.log( "Problem with path, skipping" )
+                    utils.log( "Path: %s" % repr( path ) )
+                    utils.log( "Filename: %s" % repr( path ) )
+                except:
+                    utils.log( "Problem with path, skipping" )
+                    traceback.print_exc()
         else:
             if os.path.splitext( f )[ 1 ] in xbmc.getSupportedMedia( media_type ):
                 if not contains or ( contains and ( contains in f ) ):
@@ -26,8 +35,17 @@ def absolute_listdir( path, media_type = "files", recursive = False, contains = 
 
 def absolute_folder_paths( folders, root_path ):
     actual_folders = []
+    root_path = utils.smart_unicode( root_path )
     for folder in folders:
         folder = utils.smart_unicode( folder )
-        actual_folders.append( os.path.join( root_path, folder ).replace("\\\\","\\") )
+        try:
+            actual_folders.append( os.path.join( root_path, folder ).replace("\\\\","\\") )
+        except UnicodeError:
+            utils.log( "Problem with path, skipping" )
+            utils.log( "Path: %s" % repr( root_path ) )
+            utils.log( "Folder: %s" % repr( folder ) )
+        except:
+            utils.log( "Problem with path, skipping" )
+            traceback.print_exc()
     return actual_folders
         
